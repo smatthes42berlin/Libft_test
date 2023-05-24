62be6fd4cb0923496325ae359e94d64d5c571b36
smatthes <smatthes@student.42berlin.de>
2023-05-24 13:08:32 +0200

    sf

--- ft_lst_new_test.c
+++ ft_lst_new_test.c
@@ -5,10 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: smatthes <smatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/07 10:05:53 by smatthes          #+#    #+#             */
-/*   Updated: 2023/05/07 15:53:45 by smatthes         ###   ########.fr       */
+/*   Created: 2023/05/10 08:51:21 by smatthes          #+#    #+#             */
+/*   Updated: 2023/05/21 18:34:25 by smatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "libft_test.h"
 
+int	main(void)
+{
+	t_list	*lst_ptr;
+	char	*test;
+	int		cmp_content;
+	int		cmp_next;
+	int		memcmp_res;
+
+	lst_ptr = ft_lstnew("abc");
+	cmp_content = strcmp("abc", lst_ptr->content);
+	memcmp_res = (cmp_content == 0 && lst_ptr->next == NULL) ? 0 : 1;
+	if (memcmp_res == 1)
+	{
+		printf("list node not initialised correctly");
+	}
+	compare_int_res_one_str_fail_only(0, memcmp_res, "standard 1");
+	free(lst_ptr);
+	return (1);
+}
--- ft_lstadd_front_test.c
+++ ft_lstadd_front_test.c
@@ -5,10 +5,58 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: smatthes <smatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/07 10:05:53 by smatthes          #+#    #+#             */
-/*   Updated: 2023/05/07 15:53:49 by smatthes         ###   ########.fr       */
+/*   Created: 2023/05/10 08:51:21 by smatthes          #+#    #+#             */
+/*   Updated: 2023/05/21 18:53:07 by smatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "libft_test.h"
 
+int	main(void)
+{
+	t_list	*lst_ptr;
+	t_list	*new_node1;
+	t_list	*new_node2;
+	t_list	*new_node3;
+	char	*test;
+	int		cmp_content;
+	int		cmp_next;
+	int		memcmp_res;
+
+	// test case empty list
+	new_node1 = ft_lstnew("1");
+	ft_lstadd_front(&lst_ptr, new_node1);
+	memcmp_res = (lst_ptr == new_node1) ? 0 : 1;
+	if (memcmp_res == 1)
+	{
+		printf("node not added correctly");
+	}
+	compare_int_res_one_str_fail_only(0, memcmp_res, "standard 1");
+	// test case one elem
+	new_node1 = ft_lstnew("1");
+	ft_lstadd_front(&lst_ptr, new_node1);
+	new_node2 = ft_lstnew("2");
+	ft_lstadd_front(&lst_ptr, new_node2);
+	memcmp_res = (lst_ptr == new_node2 && lst_ptr->next == new_node1) ? 0 : 1;
+	if (memcmp_res == 1)
+	{
+		printf("node not added correctly");
+	}
+	compare_int_res_one_str_fail_only(0, memcmp_res, "standard 1");
+	// test case empty list
+	new_node1 = ft_lstnew("1");
+	ft_lstadd_front(&lst_ptr, new_node1);
+	new_node2 = ft_lstnew("2");
+	ft_lstadd_front(&lst_ptr, new_node2);
+	new_node3 = ft_lstnew("3");
+	ft_lstadd_front(&lst_ptr, new_node3);
+	memcmp_res = (lst_ptr == new_node3 && lst_ptr->next == new_node2
+			&& lst_ptr->next->next == new_node1) ? 0 : 1;
+	if (memcmp_res == 1)
+	{
+		printf("node not added correctly");
+	}
+	compare_int_res_one_str_fail_only(0, memcmp_res, "standard 1");
+	return (1);
+}
